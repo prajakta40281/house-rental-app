764ef4829db50eaf29d9ecff651f8f6e6589c009
prajakta40281 <prajakta5937@gmail.com>
2026-04-16 19:53:48 +0530

    urls corrected

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://your-frontend-url.vercel.app",
+  origin: "https://house-rental-app-iota.vercel.app",
   credentials: true
 }));
 
